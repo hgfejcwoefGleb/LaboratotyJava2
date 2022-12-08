72a05b44595a3a17e7ee7b0dc03773289475b482
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2022-12-09 00:51:22 +0300

    2

--- Lr21.java
+++ Lr21.java
@@ -14,48 +14,55 @@ public class Mainjava22 {
         Pattern DifBeetDataAndRight = Pattern.compile(rightPattern);
         Pattern DifBeetDataAndwrong = Pattern.compile(wrongPattern);
         Matcher m = DifBeetDataAndRight.matcher(data);
+        Matcher d = DifBeetDataAndwrong.matcher(data);
         if (m.matches() == true)
-           System.out.println(data+"Зетс гуд");
+           System.out.println(" ");
             else {
-                Matcher c = DifBeetDataAndwrong.matcher(data);
-                    if (c.matches() == true)
-                        System.out.println("Неверный формат данных");
+                //Matcher c = DifBeetDataAndwrong.matcher(data);
+                    if (d.matches() == true)
+                        System.out.println("Введенное выражение не соответствует заданному формату даты dd/mm/yyyy");
                         else {
                             System.out.println("Обшибка");
-
             }
         }
         data.getChars(6,10,myArrayOfYear,0);
-            System.out.println(myArrayOfYear);
         int year = Integer.parseInt(String.valueOf(myArrayOfYear));
-if(year < 1900)
-    System.out.println("Введите год от 1900 до 9999");//зациклить на еще один ввод
-        System.out.println(year);
-data.getChars(3,5, myArrayOfMonth, 0);
-int month = Integer.parseInt(String.valueOf(myArrayOfMonth));
-if(month < 1 || month > 12)
-    System.out.println("Неверный формат месяца") ;//зациклить на еще один ввод
-        int c;
-        switch(month) {
-            case 1, 3, 5, 7, 8, 10, 12: c = 31;
-                break;
-            case 4, 6, 9, 11: c = 30;
-                break;
-            case 2: c = 28;
-                break;
-            default: c= 0;
-            break;
-        }
+        boolean BoolYear = year < 1900;
+          if(BoolYear == true)
+             System.out.println("Введите год от 1900 до 9999");
+             data.getChars(3,5, myArrayOfMonth, 0);
+        int month = Integer.parseInt(String.valueOf(myArrayOfMonth));
+        boolean BoolMonth = month < 1 || month > 12;
+          if(BoolMonth == true)
+             System.out.println("Неверный формат месяца") ;
+              int c;
+              switch(month) {
+                  case 1, 3, 5, 7, 8, 10, 12: c = 31;
+                    break;
+                  case 4, 6, 9, 11: c = 30;
+                    break;
+                  case 2: c = 28;
+                    break;
+                  default: c= 0;
+                    break;
+                            }
         data.getChars(0,2,myArrayOfDay,0);
         int day = Integer.parseInt(String.valueOf(myArrayOfDay));
-        if(c == 28)
-        if(day <1 || day >28)
-            System.out.println("Неверный формат дня");//зациклить на еще один ввод
-        if(c==30)
-        if(day<1||day>30)
-        System.out.println("Неверный формат дня");
-        if(c==31)
-        if(day <1 || day >31)
-            System.out.println("Неверный формат дня");
+          boolean BoolDay = day <1 || day >28;
+            if(BoolDay == true && c == 28)
+              System.out.println("Неверный формат дня");
+          boolean BoolDay1 = day <1 || day >30;
+            if(BoolDay1 == true && c == 30)
+              System.out.println("Неверный формат дня");
+          boolean BoolDay2 = day <1 || day >31;
+            if(BoolDay2 == true && c == 31)
+              System.out.println("Неверный формат дня");
+            if(c == 28 && BoolDay == false && BoolMonth == false && BoolYear == false && d.matches() == false)
+                System.out.println("Введенное выражение является датой");
+            if(c == 30 && BoolDay1 == false && BoolMonth == false && BoolYear == false && d.matches() == false)
+                System.out.println("Введенное выражение является датой");
+            if(c == 31 && BoolDay2 == false && BoolMonth == false && BoolYear == false && d.matches() == false)
+                System.out.println("Введенное выражение является датой");
+sc.close();
     }
 }
