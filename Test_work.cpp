532906370ae1076c640ff8e0495bf1f8c09ee5c9
hgfejcwoefGleb <114211284+hgfejcwoefGleb@users.noreply.github.com>
2024-02-03 14:31:27 +0300

    Update Test_work.cpp

--- Test_work.cpp
+++ Test_work.cpp
@@ -1,39 +1,74 @@
 ﻿// Test_work.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+//
+
 #include <iostream>
+#include <string>
 struct User {
     int id;
-    char name;
+    std::string name;
     int age;
 
 };
 User* user_arr = new User[100];
 int num_of_acc = 0;
-User empty_acc;
+
 
 int main()
 
 {
-    std::cout << user_arr[0];
+    static User empty_acc;
+    empty_acc.age = 0;
+    empty_acc.id = 0;
+    empty_acc.name = "";
+    for (int i = 0; i < 100; i++) {
+        user_arr[i] = empty_acc;
+    }
+    while (true) {
+        std::string func_chosen = "";
+        std::cout << "Enter disireable action add or del or up or show" << std::endl;
+        std::cin >> func_chosen;
+        do_smth()
+       
+    }
     
 }
-void add_new_account(int id, char name, int age, User* arr, int &num_of_acc, User login) {
+void do_smth(int id, std::string name, int age, User* arr, int& num_of_acc, User login, User empty_acc, void(*func)(int id, std::string name, int age, User* arr, int& num_of_acc, User login, User empty_acc), std::string func_chosen) {
+    void (*add)(int id, std::string name, int age, User * arr, int& num_of_acc, User login, User empty_acc) = add_new_account;
+    void (*del)(int id, std::string name, int age, User * arr, int& num_of_acc, User login, User empty_acc) = del_acc;
+    void (*up)(int id, std::string name, int age, User * arr, int& num_of_acc, User login, User empty_acc) = upload_data;
+    void (*show)(User * arr) = show;
+    if (func_chosen == "add") {
+        add(id, name, age, arr, num_of_acc, login, empty_acc);
+        
+    }
+}
+void add_new_account(int id, std::string name, int age, User* arr, int& num_of_acc, User login, User empty_acc) {
     login.age = age;
     login.id = num_of_acc;
     login.name = name;
     arr[num_of_acc] = login;
     num_of_acc += 1;
 }
-void del_acc(User login, User* arr) {
+void del_acc(int id, std::string name, int age, User* arr, int& num_of_acc, User login, User empty_acc) {
     arr[login.id] = empty_acc;
 }
-void upload_data(int id, char name, int age, User login) {
+void upload_data(int id, std::string name, int age, User* arr, int& num_of_acc, User login, User empty_acc) {
     login.id = id;
     login.name = name;
     login.age = age;
 }
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+void show_data(int id, std::string name, int age, User* arr, int& num_of_acc, User login, User empty_acc) {
+    for (int i = 0; i < 100; i++) {
+        if (arr[i].age != 0) {
+            std::cout << "User name is " << arr[i].name << std::endl;
+            std::cout << "User age is " << arr[i].age << std::endl;
+            std::cout << "User id is " << arr[i].id << std::endl;
+        }
+    }
+}
+//  программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
 // Советы по началу работы 
